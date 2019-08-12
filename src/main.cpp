c27be86c983633e8fdf945e8c806275f0ef37950
peterrum <peterrmuench@gmail.com>
2019-08-12 13:32:06 +0200

    Simplify code

--- src/main.cpp
+++ src/main.cpp
@@ -72,10 +72,10 @@ public:
   }
 
   void
-  compute_diag_local(const MatrixFree<dim, number> &                   data,
-                     LinearAlgebra::distributed::Vector<number> &      dst,
-                     const LinearAlgebra::distributed::Vector<number> &src,
-                     const std::pair<unsigned int, unsigned int> &     cell_range) const
+  compute_diag_local(const MatrixFree<dim, number> &             data,
+                     LinearAlgebra::distributed::Vector<number> &dst,
+                     const LinearAlgebra::distributed::Vector<number> & /*src*/,
+                     const std::pair<unsigned int, unsigned int> &cell_range) const
   {
     const bool is_mg = false;
     const bool is_dg = (data.get_dof_handler().get_fe().dofs_per_vertex == 0);
@@ -88,100 +88,86 @@ public:
         fe_eval.reinit(cell);
         fe_eval_plain.reinit(cell);
 
+        // 1) initialize empty local diagonal
         AlignedVector<VectorizedArray<number>> diagonal(fe_eval.dofs_per_cell,
                                                         make_vectorized_array<number>(0.0));
 
+        // 2) get DoF-indices
+        std::vector<types::global_dof_index> dof_indices[VectorizedArray<number>::n_array_elements];
+        for (unsigned int v = 0; v < data.n_components_filled(cell); v++)
+          {
+            dof_indices[v].resize(fe_eval.dofs_per_cell);
+
+            auto cell_v = data.get_cell_iterator(cell, v);
+
+            if (is_mg)
+              cell_v->get_mg_dof_indices(dof_indices[v]);
+            else
+              cell_v->get_dof_indices(dof_indices[v]);
+
+            if (!is_dg)
+              {
+                // in the case of CG: shape functions are not ordered
+                // lexicographically see
+                // (https://www.dealii.org/8.5.1/doxygen/deal.II/classFE__Q.html)
+                // so we have to fix the order
+                auto temp = dof_indices[v];
+                for (unsigned int j = 0; j < dof_indices[v].size(); j++)
+                  dof_indices[v][j] = temp[data.get_shape_info().lexicographic_numbering[j]];
+              }
+          }
+
+        // 3) loop over all local DoFs and setup local diagonal entry by entry
         for (unsigned int i = 0; i < fe_eval.dofs_per_cell; ++i)
           {
+            // 3a) zero out local source vector
             for (unsigned int j = 0; j < fe_eval.dofs_per_cell; ++j)
               fe_eval.begin_dof_values()[j] = make_vectorized_array<number>(0.0);
 
+            // 3b) create standard basis taking care constraints
             for (unsigned int v = 0; v < data.n_components_filled(cell); v++)
-              {
-                auto cell_v = data.get_cell_iterator(cell, v);
-
-                std::vector<types::global_dof_index> dof_indices(fe_eval.dofs_per_cell);
-                if (is_mg)
-                  cell_v->get_mg_dof_indices(dof_indices);
-                else
-                  cell_v->get_dof_indices(dof_indices);
-
-                if (!is_dg)
-                  {
-                    // in the case of CG: shape functions are not ordered
-                    // lexicographically see
-                    // (https://www.dealii.org/8.5.1/doxygen/deal.II/classFE__Q.html)
-                    // so we have to fix the order
-                    auto temp = dof_indices;
-                    for (unsigned int j = 0; j < dof_indices.size(); j++)
-                      dof_indices[j] = temp[data.get_shape_info().lexicographic_numbering[j]];
-                  }
-
-                if (!constraints.is_constrained(dof_indices[i]))
+                if (!constraints.is_constrained(dof_indices[v][i]))
                   {
                     fe_eval.begin_dof_values()[i][v] = 1.0;
 
-                    for (unsigned int ii = 0; ii < dof_indices.size(); ii++)
+                    for (unsigned int ii = 0; ii < dof_indices[v].size(); ii++)
                       {
-                        if (!constraints.is_constrained(dof_indices[ii]))
+                        if (!constraints.is_constrained(dof_indices[v][ii]))
                           continue;
-                        auto &cs = *constraints.get_constraint_entries(dof_indices[ii]);
+                        auto &cs = *constraints.get_constraint_entries(dof_indices[v][ii]);
                         for (auto c : cs)
-                          {
-                            if (c.first == dof_indices[i])
+                            if (c.first == dof_indices[v][i])
                               fe_eval.begin_dof_values()[ii][v] = c.second;
-                          }
                       }
                   }
-              }
 
+            // 3c) perform stand matrix-free operation
             fe_eval.evaluate(false, true, false);
             for (unsigned int q = 0; q < fe_eval.n_q_points; ++q)
               fe_eval.submit_gradient(fe_eval.get_gradient(q), q);
             fe_eval.integrate(false, true);
 
+            // 3d)
             VectorizedArray<number> temp = 0.0;
-
             for (unsigned int v = 0; v < data.n_components_filled(cell); v++)
-              {
-                auto cell_v = data.get_cell_iterator(cell, v);
-
-                std::vector<types::global_dof_index> dof_indices(fe_eval.dofs_per_cell);
-                if (is_mg)
-                  cell_v->get_mg_dof_indices(dof_indices);
-                else
-                  cell_v->get_dof_indices(dof_indices);
-
-                if (!is_dg)
-                  {
-                    // in the case of CG: shape functions are not ordered
-                    // lexicographically see
-                    // (https://www.dealii.org/8.5.1/doxygen/deal.II/classFE__Q.html)
-                    // so we have to fix the order
-                    auto temp = dof_indices;
-                    for (unsigned int j = 0; j < dof_indices.size(); j++)
-                      dof_indices[j] = temp[data.get_shape_info().lexicographic_numbering[j]];
-                  }
-
-                if (!constraints.is_constrained(dof_indices[i]))
+                if (!constraints.is_constrained(dof_indices[v][i]))
                   {
                     temp = fe_eval.begin_dof_values()[i][v];
 
-                    for (unsigned int ii = 0; ii < dof_indices.size(); ii++)
+                    for (unsigned int ii = 0; ii < dof_indices[v].size(); ii++)
                       {
-                        if (!constraints.is_constrained(dof_indices[ii]))
+                        if (!constraints.is_constrained(dof_indices[v][ii]))
                           continue;
-                        auto &cs = *constraints.get_constraint_entries(dof_indices[ii]);
+                        auto &cs = *constraints.get_constraint_entries(dof_indices[v][ii]);
                         for (auto c : cs)
-                          {
-                            if (c.first == dof_indices[i])
+                            if (c.first == dof_indices[v][i])
                               temp += fe_eval.begin_dof_values()[ii][v] * c.second;
-                          }
                       }
                   }
-              }
             diagonal[i] = temp;
           }
+
+        // 4) write local diagonal back to the global diagonal
         for (unsigned int i = 0; i < fe_eval_plain.dofs_per_cell; ++i)
           fe_eval_plain.begin_dof_values()[i] = diagonal[i];
         fe_eval_plain.distribute_local_to_global(dst);
